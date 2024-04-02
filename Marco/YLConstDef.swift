f91fb90e3570e84892f24f66f17895f734888cee
benjiang <123123@qq.com>
2024-04-02 13:39:32 +0800

    bug fix

--- Marco/YLConstDef.swift
+++ Marco/YLConstDef.swift
@@ -22,27 +22,33 @@ let YLAlphaNum: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01
 let YLApplictionShared = UIApplication.shared
 let YLAppDelegate = YLApplictionShared.delegate as! AppDelegate
 let YLKeyWindow = YLApplictionShared.windows[0]
-let YLNotificationCenter = Foundation.NotificationCenter.default
-let YLUserDefaults = Foundation.UserDefaults.standard
-let YLMainBundle = Bundle.main;
+let YLNotificationCenter = NotificationCenter.default
+let YLUserDefaults = UserDefaults.standard
+let YLMainBundle = Bundle.main
+
+// MARK: info.plist
+let YLInfoDictionary = YLMainBundle.infoDictionary!
+
+/* CFBundleDisplayName*/
+let YLBundleDisplayName: String = YLInfoDictionary["CFBundleDisplayName"] as! String
+let YLProjectName: String = YLInfoDictionary[kCFBundleExecutableKey as String] as! String
+let YLBundleName: String = YLInfoDictionary[kCFBundleNameKey as String] as! String
+let YLBundleIdentifier: String = Bundle.main.bundleIdentifier!
+let YLBundleShortVersion: String = YLInfoDictionary["CFBundleShortVersionString"] as! String
+let YLBundleVersion: String = YLInfoDictionary["CFBundleVersion"] as! String
 
 // MARK: Device
 let YLCurrentDevice = UIDevice.current
-
 let YLDeviceName: String = YLCurrentDevice.name
-
 let YLDeviceSystemName: String = YLCurrentDevice.systemName
-
 let YLDeviceVersion = (YLCurrentDevice.systemVersion as NSString).floatValue
-
 let YLDeviceModel: String = YLCurrentDevice.model
-
 let YLDeviceLanguage: String = Locale.preferredLanguages.first!
-
 let YLLocaleIdentifier: String = Locale.current.identifier
 
 let YL_IS_IPhoneX_All = UIDevice.isIPhoneXSeries()
 
+// MARK: UI
 let YLScreenBounds = UIScreen.main.bounds
 let YLScreenSize = YLScreenBounds.size
 let YLScreenWidth = YLScreenSize.width
@@ -60,6 +66,7 @@ let YLSafeAreaHeightWithTabbar: CGFloat = (YLScreenHeight-YLSafeAreaTop-YLTabbar
 //全面屏iPhoneTabBar额外的高度
 let YLFullTabbarExtraHeight: CGFloat = (YL_IS_IPhoneX_All ? 34 : 0)
 
+
 func YLDesignFit(_ width: CGFloat) -> CGFloat {
     let mainSize =  UIScreen.main.bounds.size
     let min = mainSize.height < mainSize.width ? mainSize.height : mainSize.width
@@ -69,6 +76,18 @@ func YLDesignFit(_ width: CGFloat) -> CGFloat {
 func YLDesignFit(_ rect: CGRect) -> CGRect {
     let fixY = rect.origin.y - YLNavBarHeight
     return CGRect(x: rect.origin.x, y: fixY, width: rect.size.width, height: rect.size.height)
+}
+
+func YLSetTabbarSelectedIndex(_ index: Int) {
+    if let currentNav = UIViewController.currentViewController()?.navigationController {
+        if (currentNav.viewControllers.count > 1) {
+            currentNav.popToRootViewController(animated: false)
+        }
+    }
+    guard let tabBarVc = UIApplication.shared.windows[0].rootViewController as? UITabBarController else { return }
+    DispatchQueue.async_after(.now() + 0.01) {
+        tabBarVc.selectedIndex = index
+    }
 }
 
 func YLAudioServicesPlayAlertSound(_ path: String) {
@@ -120,7 +139,6 @@ func kDINCondBoldFont(_ size: CGFloat) -> UIFont {
 }
 
 // MARK: COLOR
-
 let YLThemeBlueColor = UIColor.colorWithHexString("1BC5E8")
 
 // MARK: UIIMage
@@ -131,11 +149,11 @@ func YLImageNamed(_ imageName: String?) -> UIImage {
 }
 
 func YLImageOfFile(_ imageName: String) -> UIImage {
-    let file = "\(Bundle.main.bundlePath)/\(imageName)"
+    let file = "\(YLMainBundle.bundlePath)/\(imageName)"
     guard let image = UIImage(contentsOfFile: file) else { return UIImage() }
     return image.withRenderingMode(.alwaysOriginal)
 }
 
-func YLMainBundlePath(_ name: String, ofType: String?) -> String {
-    return Bundle.main.path(forResource: name, ofType: ofType) ?? ""
+func YLMainBundlePath(_ name: String, ofType: String? = nil) -> String {
+    return YLMainBundle.path(forResource: name, ofType: ofType) ?? ""
 }
